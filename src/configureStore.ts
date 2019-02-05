f952219ee1c57adc9ae7258e307157bf29f72e75
Erick Tamayo <ericktamayo@gmail.com>
2019-02-05 16:02:36 -0400

    simplified store initialization

--- src/configureStore.ts
+++
@@ -1,5 +0,0 @@
-import { createStore, applyMiddleware, Store } from 'redux'
-import thunk from 'redux-thunk'
-import app from './reducers'
-
-export default (): Store => createStore(app, applyMiddleware(thunk))
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
-
 import { Provider } from 'react-redux'
-import configureStore from './configureStore'
 import StartScreen from './features/start-screen/containers/StartScreen'
+import { configureStore } from './store'
 
 const store = configureStore()
 
