7f01210a386a32459a537d3fe856d50620f9b17c
Erick Tamayo <ericktamayo@gmail.com>
2019-02-05 02:04:21 -0400

    fixed test

--- src/features/start-screen/components/__tests__/StartScreen.test.tsx
+++ src/features/start-screen/components/__tests__/StartScreen.test.tsx
@@ -2,10 +2,12 @@ import 'react-native'
 import * as React from 'react'
 import { shallow } from 'enzyme'
 import StartScreen from '../StartScreen'
+import { AddCounter } from '../../actions'
 
 describe('<StartScreen />', () => {
   it('renders correctly', () => {
-    const wrapper = shallow(<StartScreen counter={0} />)
+    const addCounter = () => ({} as AddCounter)
+    const wrapper = shallow(<StartScreen counter={0} addCounter={addCounter} />)
     expect(wrapper).toMatchSnapshot()
   })
 })
